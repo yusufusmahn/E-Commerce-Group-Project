66f72b8107d1cc6269fa27c0f1a37ce1a3872fc3
yusufusman <yusuf.usman1003@gmail.com>
2025-06-23 13:36:26 +0100

    update

---
+++ src/main/java/org/temu/data/models/User.java
@@ -0,0 +1,18 @@
+package org.temu.data.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+}
+
+
