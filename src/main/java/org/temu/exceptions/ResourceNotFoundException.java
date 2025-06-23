27cdfe6c909bbe96a71ed00db67aaacd8284b912
yusufusman <yusuf.usman1003@gmail.com>
2025-06-23 14:01:46 +0100

    update

---
+++ src/main/java/org/temu/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package org.temu.exceptions;
+
+public class ResourceNotFoundException extends TemuException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
