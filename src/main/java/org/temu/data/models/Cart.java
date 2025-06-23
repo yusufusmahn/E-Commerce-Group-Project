1d2e1e6271585987a982cc46b92d070be7fc43ee
yusufusman <yusuf.usman1003@gmail.com>
2025-06-23 14:26:12 +0100

    update

--- src/main/java/org/temu/data/models/Cart.java
+++ src/main/java/org/temu/data/models/Cart.java
@@ -1,10 +1,12 @@
 package org.temu.data.models;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Data
 @Document(collection = "carts")
 public class Cart {
     @Id
