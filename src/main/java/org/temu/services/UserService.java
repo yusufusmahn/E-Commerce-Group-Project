a3cc052c486f1bbc6426d35e6651d33c71a2d5f0
yusufusman <yusuf.usman1003@gmail.com>
2025-06-23 15:19:15 +0100

    update

---
+++ src/main/java/org/temu/services/UserService.java
@@ -0,0 +1,11 @@
+package org.temu.services;
+
+
+import org.temu.dtos.requests.*;
+import org.temu.dtos.responses.*;
+
+public interface UserService {
+    UserResponse registerUser(RegisterUserRequest request);
+    UserResponse loginUser(LoginUserRequest request);
+    UserResponse getUserById(String id);
+}
