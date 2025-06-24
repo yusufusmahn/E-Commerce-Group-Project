f1b585128b1a56adb0584fffc44c17debc8feeed
yusufusman <yusuf.usman1003@gmail.com>
2025-06-24 10:19:03 +0100

    update

--- src/main/java/org/temu/dtos/requests/CreateProductRequest.java
+++ src/main/java/org/temu/dtos/requests/CreateProductRequest.java
@@ -1,10 +1,17 @@
 package org.temu.dtos.requests;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CreateProductRequest {
+    @NotBlank(message = "Product name is required.")
     private String name;
+
+    @NotBlank(message = "Product description is required.")
     private String description;
-    private double price;
+
+    @NotNull(message = "Price is required.")
+    @Positive(message = "Price must be greater than zero.")
+    private Double price;
 }
--- src/main/java/org/temu/dtos/requests/LoginUserRequest.java
+++ src/main/java/org/temu/dtos/requests/LoginUserRequest.java
@@ -1,10 +1,15 @@
 package org.temu.dtos.requests;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class LoginUserRequest {
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
     private String password;
 
 }
