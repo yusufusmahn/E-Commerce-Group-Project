02c36d71888b00cb9f1e16e676987fef2ace8803
yusufusman <yusuf.usman1003@gmail.com>
2025-06-24 10:19:18 +0100

    update

---
+++ src/main/java/org/temu/data/models/Product.java
@@ -0,0 +1,18 @@
+package org.temu.data.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantityAvailable;
+
+}
