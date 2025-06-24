1a5155450cbc68c92c930b37e2daab389818304f
yusufusman <yusuf.usman1003@gmail.com>
2025-06-24 10:19:28 +0100

    update

--- src/main/java/org/temu/dtos/requests/RegisterUserRequest.java
+++ src/main/java/org/temu/dtos/requests/RegisterUserRequest.java
@@ -1,11 +1,21 @@
 package org.temu.dtos.requests;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequest {
+
+    @NotBlank(message = "Name is required.")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters.")
     private String name;
+
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
     private String password;
 
 }
