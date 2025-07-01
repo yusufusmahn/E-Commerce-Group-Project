a585a9230c03704c069e25a79d3a4f4b500b12f7
yusufusman <yusuf.usman1003@gmail.com>
2025-07-01 13:49:06 +0100

    update

---
+++ src/main/java/org/jumia/services/CloudinaryService/CloudinaryService.java
@@ -0,0 +1,22 @@
+package org.jumia.services.CloudinaryService;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        return uploadResult.get("url").toString();
+    }
+}
