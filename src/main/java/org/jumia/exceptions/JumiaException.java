fbc53953dd6b311d0f1823e1dab47d0612bd3856
yusufusman <yusuf.usman1003@gmail.com>
2025-07-01 13:43:37 +0100

    update

---
+++ src/main/java/org/jumia/exceptions/JumiaException.java
@@ -0,0 +1,8 @@
+package org.jumia.exceptions;
+
+public class JumiaException extends RuntimeException {
+    public JumiaException(String message) {
+        super(message);
+    }
+}
+
--- src/main/java/org/temu/exceptions/TemuException.java
+++
@@ -1,7 +0,0 @@
-package org.temu.exceptions;
-
-public class TemuException extends RuntimeException {
-    public TemuException(String message) {
-        super(message);
-    }
-}
