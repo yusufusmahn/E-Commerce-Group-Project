08385f378de7abb113fcf76226609aeecc9e8e8e
yusufusman <yusuf.usman1003@gmail.com>
2025-07-01 13:43:48 +0100

    update

---
+++ src/main/java/org/jumia/security/JwtUtil.java
@@ -0,0 +1,90 @@
+package org.jumia.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 10;
+
+
+    public String extractUsername(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        if (token == null || !token.contains(".")) {
+            throw new IllegalArgumentException("Invalid JWT token");
+        }
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * Generate a new JWT token for the given username.
+     *
+     * @param username the username to include in the token
+     * @return the generated JWT token
+     */
+    public String generateToken(String username, List<String> roles) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles) // Add roles as a claim
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS512)
+                .compact();
+
+    }
+
+
+    public boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+
+
+    public long getExpirationTime() {
+        return EXPIRATION_TIME;
+    }
+
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public List<String> extractRoles(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.get("roles", List.class); // Retrieve the roles claim
+    }
+
+
+
+}
