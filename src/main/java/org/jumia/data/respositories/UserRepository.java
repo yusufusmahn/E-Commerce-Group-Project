5a968ebfe2bbca143554989c3c85b3b805caab9d
yusufusman <yusuf.usman1003@gmail.com>
2025-07-01 13:50:16 +0100

    update

---
+++ src/main/java/org/jumia/data/respositories/UserRepository.java
@@ -0,0 +1,22 @@
+package org.jumia.data.respositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.jumia.data.models.Role;
+import org.jumia.data.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    List<User> findByRolesContaining(Role role);
+    Optional<User> findById(String id);
+    // Find users by email containing a substring
+    List<User> findByEmailContainingIgnoreCase(String email);
+
+    // Find users by email containing a substring and role
+    List<User> findByEmailContainingIgnoreCaseAndRolesContaining(String email, Role role);
+
+
+
+}
