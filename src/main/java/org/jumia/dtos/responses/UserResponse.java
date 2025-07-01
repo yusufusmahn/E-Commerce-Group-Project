91e43700532646f228ffe16d2b568402071b88b6
yusufusman <yusuf.usman1003@gmail.com>
2025-07-01 14:01:44 +0100

    update

--- src/main/java/org/jumia/dtos/responses/UserResponse.java
+++ src/main/java/org/jumia/dtos/responses/UserResponse.java
@@ -1,11 +1,15 @@
-package org.temu.dtos.responses;
+package org.jumia.dtos.responses;
 
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class UserResponse {
     private String id;
     private String name;
     private String email;
+    private Set<String> roles; // Add this field
+
 
 }
