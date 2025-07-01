cabcc1c53fe216856f57c61a9924c85fcb4ad15d
yusufusman <yusuf.usman1003@gmail.com>
2025-07-01 13:45:04 +0100

    update

---
+++ src/main/java/org/jumia/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package org.jumia.exceptions;
+
+public class ResourceNotFoundException extends JumiaException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/temu/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.temu.exceptions;
-
-public class ResourceNotFoundException extends TemuException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
