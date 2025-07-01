e1358650616d6edf06abf08aa7c00e948691e4b3
yusufusman <yusuf.usman1003@gmail.com>
2025-07-01 13:44:27 +0100

    update

---
+++ src/main/java/org/jumia/data/respositories/ProductRepository.java
@@ -0,0 +1,18 @@
+package org.jumia.data.respositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.jumia.data.models.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findBySellerId(String sellerId);
+    long countBySellerId(String sellerId);
+    List<Product> findByNameContainingIgnoreCase(String keyword);
+    List<Product> findAll();
+    Optional<Product> findByIdAndSellerId(String productId, String sellerId);
+
+
+
+}
