45cf009223cc663b16d75f6a1ea0079dc351b81d
yusufusman <yusuf.usman1003@gmail.com>
2025-07-04 11:57:32 +0100

    update

---
+++ src/main/java/org/jumia/controllers/Admin/AdminCartController.java
@@ -0,0 +1,35 @@
+package org.jumia.controllers.admin;
+
+import org.jumia.dtos.responses.CartResponse;
+import org.jumia.services.admin.AdminCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/carts")
+public class AdminCartController {
+
+    @Autowired
+    private AdminCartService adminCartService;
+
+    @GetMapping
+    public ResponseEntity<List<CartResponse>> getAllCarts() {
+        List<CartResponse> carts = adminCartService.getAllCarts();
+        return ResponseEntity.ok(carts);
+    }
+
+    @DeleteMapping("/{userId}/clear")
+    public ResponseEntity<?> clearCartForUser(@PathVariable String userId) {
+        adminCartService.clearCartForUser(userId);
+        return ResponseEntity.ok("Cart cleared for user ID: " + userId);
+    }
+
+    @DeleteMapping("/remove-product/{productId}")
+    public ResponseEntity<?> removeProductFromAllCarts(@PathVariable String productId) {
+        adminCartService.removeProductFromAllCarts(productId);
+        return ResponseEntity.ok("Product removed from all carts: " + productId);
+    }
+}
