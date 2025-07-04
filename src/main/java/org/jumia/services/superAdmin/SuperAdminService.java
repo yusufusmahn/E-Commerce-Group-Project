3082b9d03284c21c54d9e7f353bed80effbe1d91
yusufusman <yusuf.usman1003@gmail.com>
2025-07-04 12:28:00 +0100

    update

--- src/main/java/org/jumia/services/superAdmin/SuperAdminService.java
+++ src/main/java/org/jumia/services/superAdmin/SuperAdminService.java
@@ -1,11 +1,21 @@
 package org.jumia.services.superAdmin;
 
 import org.jumia.dtos.requests.PromoteToAdminRequest;
+import org.jumia.dtos.requests.PromoteToSellerRequest;
 import org.jumia.dtos.requests.RevokeAdminRoleRequest;
+import org.jumia.dtos.requests.RevokeSellerRoleRequest;
 import org.jumia.dtos.responses.UserResponse;
 import org.jumia.data.models.User;
 
 public interface SuperAdminService {
-    UserResponse promoteToAdmin(PromoteToAdminRequest request, User currentUser);
-    UserResponse revokeAdminRole(RevokeAdminRoleRequest request, User currentUser);
+//    UserResponse promoteToAdmin(PromoteToAdminRequest request, User currentUser);
+//    UserResponse revokeAdminRole(RevokeAdminRoleRequest request, User currentUser);
+
+    UserResponse promoteToAdmin(PromoteToAdminRequest request);
+
+    UserResponse revokeAdminRole(RevokeAdminRoleRequest request);
+    UserResponse promoteToSeller(PromoteToSellerRequest request);
+    UserResponse revokeSellerRole(RevokeSellerRoleRequest request);
+
+
 }
