e2da986bb6910e13eca37ba3304de39bc5eaf777
yusufusman <yusuf.usman1003@gmail.com>
2025-07-04 11:59:18 +0100

    update

--- src/main/java/org/jumia/dtos/responses/UserResponse.java
+++ src/main/java/org/jumia/dtos/responses/UserResponse.java
@@ -1,15 +1,22 @@
 package org.jumia.dtos.responses;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import java.util.List;
 import java.util.Set;
 
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class UserResponse {
     private String id;
     private String name;
     private String email;
-    private Set<String> roles; // Add this field
+//    private Set<String> roles;
+    private List<String> roles;
+    private String storeName;
+    private String contactInfo;
+    private String description;// Add this field
 
 
 }
