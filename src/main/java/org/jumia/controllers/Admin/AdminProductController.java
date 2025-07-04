8050ee6f268470c582579303cd0fc3ae2dcbbe29
yusufusman <yusuf.usman1003@gmail.com>
2025-07-04 13:34:04 +0100

    update

--- src/main/java/org/jumia/controllers/Admin/AdminProductController.java
+++ src/main/java/org/jumia/controllers/Admin/AdminProductController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 @RestController
@@ -18,19 +20,40 @@ public class AdminProductController {
     @Autowired
     private AdminProductService adminProductService;
 
-    // 🔐 SUPER_ADMIN only
-    @PostMapping
-    public ResponseEntity<ProductResponse> addProduct(@Valid @RequestBody CreateProductRequest request) {
-        ProductResponse response = adminProductService.addProductAsAdmin(request);
+
+    @PostMapping(value = "/addProduct", consumes = {"multipart/form-data"})
+    public ResponseEntity<ProductResponse> addProduct(
+            @ModelAttribute @Valid CreateProductRequest request,
+            @RequestPart(value = "image", required = false) MultipartFile image
+    ) {
+        ProductResponse response = adminProductService.addProductAsAdmin(request, image);
         return ResponseEntity.ok(response);
     }
 
-    // 🔐 ADMIN or SUPER_ADMIN
-    @PutMapping("/{id}")
-    public ResponseEntity<ProductResponse> updateProduct(@PathVariable String id, @Valid @RequestBody UpdateProductRequest request) {
-        ProductResponse updated = adminProductService.updateProductAsAdmin(id, request);
+    @PutMapping(value = "/{id}", consumes = {"multipart/form-data"})
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable String id,
+            @ModelAttribute @Valid UpdateProductRequest request,
+            @RequestPart(value = "image", required = false) MultipartFile image
+    ) {
+        ProductResponse updated = adminProductService.updateProductAsAdmin(id, request, image);
         return ResponseEntity.ok(updated);
     }
+
+
+    // 🔐 SUPER_ADMIN only
+//    @PostMapping
+//    public ResponseEntity<ProductResponse> addProduct(@Valid @RequestBody CreateProductRequest request) {
+//        ProductResponse response = adminProductService.addProductAsAdmin(request);
+//        return ResponseEntity.ok(response);
+//    }
+//
+//    // 🔐 ADMIN or SUPER_ADMIN
+//    @PutMapping("/{id}")
+//    public ResponseEntity<ProductResponse> updateProduct(@PathVariable String id, @Valid @RequestBody UpdateProductRequest request) {
+//        ProductResponse updated = adminProductService.updateProductAsAdmin(id, request);
+//        return ResponseEntity.ok(updated);
+//    }
 
     // 🔐 SUPER_ADMIN only
     @DeleteMapping("/{id}")
