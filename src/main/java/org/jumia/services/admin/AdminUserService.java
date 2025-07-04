53f9073e4188a1434a4f71de9590dd41079645be
yusufusman <yusuf.usman1003@gmail.com>
2025-07-04 14:41:55 +0100

    update

---
+++ src/main/java/org/jumia/services/admin/AdminUserService.java
@@ -0,0 +1,23 @@
+package org.jumia.services.admin;
+
+import org.jumia.dtos.requests.DeleteUsersRequest;
+import org.jumia.dtos.requests.UserSearchRequest;
+import org.jumia.dtos.responses.*;
+
+import java.util.List;
+
+public interface AdminUserService {
+
+    // User management
+    List<UserResponse> getAllUsers();
+
+    UserResponse getUserById(String id);
+
+    void deleteUserById(String id);
+
+    void deleteUsers(DeleteUsersRequest request);
+
+    List<UserResponse> searchUsers(UserSearchRequest userSearchRequest);
+}
+
+
