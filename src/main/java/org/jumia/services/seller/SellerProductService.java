84675b66b5fa68f445431cc68067aa2ba446362c
yusufusman <yusuf.usman1003@gmail.com>
2025-07-04 12:16:11 +0100

    update

--- src/main/java/org/jumia/services/seller/SellerProductService.java
+++ src/main/java/org/jumia/services/seller/SellerProductService.java
@@ -1,3 +1,22 @@
+//package org.jumia.services.seller;
+//
+//import org.jumia.dtos.requests.CreateProductRequest;
+//import org.jumia.dtos.requests.UpdateProductRequest;
+//import org.jumia.dtos.requests.UpdateStockRequest;
+//import org.jumia.dtos.responses.ProductResponse;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.util.List;
+//
+//public interface SellerProductService {
+//    ProductResponse addProduct(CreateProductRequest request, MultipartFile image, String sellerId);
+//    ProductResponse updateProduct(String productId, UpdateProductRequest request, MultipartFile image, String sellerId);
+//    void deleteProduct(String productId, String sellerId);
+//    List<ProductResponse> getProductsBySellerId(String sellerId);
+//    ProductResponse getProductById(String productId, String sellerId);
+//    List<ProductResponse> getAllProducts();
+//    ProductResponse updateStock(UpdateStockRequest request, String sellerId);
+//}
 package org.jumia.services.seller;
 
 import org.jumia.dtos.requests.CreateProductRequest;
@@ -9,11 +28,18 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface SellerProductService {
-    ProductResponse addProduct(CreateProductRequest request, MultipartFile image, String sellerId);
-    ProductResponse updateProduct(String productId, UpdateProductRequest request, MultipartFile image, String sellerId);
-    void deleteProduct(String productId, String sellerId);
-    List<ProductResponse> getProductsBySellerId(String sellerId);
-    ProductResponse getProductById(String productId, String sellerId);
+
+    ProductResponse addProduct(CreateProductRequest request, MultipartFile image);
+
+    ProductResponse updateProduct(String productId, UpdateProductRequest request, MultipartFile image);
+
+    void deleteProduct(String productId);
+
+    List<ProductResponse> getProductsBySellerId();
+
+    ProductResponse getProductById(String productId);
+
+    ProductResponse updateStock(UpdateStockRequest request);
+
     List<ProductResponse> getAllProducts();
-    ProductResponse updateStock(UpdateStockRequest request, String sellerId);
 }
