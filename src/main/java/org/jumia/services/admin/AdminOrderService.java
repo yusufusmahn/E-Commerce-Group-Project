7172458634ac56e5ff63925dc69f25167f8b1b46
yusufusman <yusuf.usman1003@gmail.com>
2025-07-04 13:04:31 +0100

    update

---
+++ src/main/java/org/jumia/services/admin/AdminOrderService.java
@@ -0,0 +1,17 @@
+package org.jumia.services.admin;
+
+import org.springframework.security.core.Authentication;
+import org.jumia.dtos.requests.UpdateOrderRequest;
+import org.jumia.dtos.responses.OrderResponse;
+
+import java.util.List;
+
+public interface AdminOrderService {
+    OrderResponse updateOrderAsAdmin(String id, UpdateOrderRequest request);
+    void cancelOrderAsAdmin(String id);
+    List<OrderResponse> getAllOrders();
+    List<OrderResponse> getOrdersByUserId(String userId);
+    void validateAdminRole(Authentication authentication);
+    void deleteOrder(String id);
+
+}
