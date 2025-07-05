f167c409eb1bb33d7f8a9411087f808e55e4deac
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 00:07:56 +0100

    update

--- src/main/java/org/jumia/dtos/requests/AddToCartRequest.java
+++ src/main/java/org/jumia/dtos/requests/AddToCartRequest.java
@@ -1,16 +1,15 @@
 package org.jumia.dtos.requests;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AddToCartRequest {
-    @NotBlank(message = "User ID is required.")
-    private String userId;
 
     @NotBlank(message = "Product ID is required.")
     private String productId;
 
-//    private String userEmail; // Add this field
-
+    @Min(value = 1, message = "Quantity must be at least 1.")
+    private int quantity;
 }
