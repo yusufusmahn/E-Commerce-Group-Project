8310b2712576e0f14b489042afd0e669c78b80c6
yusufusman <yusuf.usman1003@gmail.com>
2025-07-05 10:51:34 +0100

    update

--- src/main/java/org/jumia/data/respositories/OrderRepository.java
+++ src/main/java/org/jumia/data/respositories/OrderRepository.java
@@ -1,5 +1,6 @@
 package org.jumia.data.respositories;
 
+import org.jumia.data.models.OrderStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.jumia.data.models.Order;
 
@@ -10,7 +11,9 @@ public interface OrderRepository extends MongoRepository<Order, String> {
     List<Order> findByUserId(String userId);
     List<Order> findBySellerIdAndCreatedAtBetween(String sellerId, LocalDateTime startDate, LocalDateTime endDate);
     long countBySellerId(String userId);
-    List<Order> findBySellerIdAndStatus(String sellerId, String status);
+    List<Order> findBySellerIdAndStatus(String sellerId, OrderStatus status);
     List<Order> findBySellerId(String sellerId);
+    List<Order> findByUserIdAndStatus(String userId, OrderStatus status);
+
 
 }
