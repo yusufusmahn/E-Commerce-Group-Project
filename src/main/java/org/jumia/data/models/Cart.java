f7e73b334395f57bc51db91da0470ffeb1f95c7a
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 00:11:29 +0100

    update

--- src/main/java/org/jumia/data/models/Cart.java
+++ src/main/java/org/jumia/data/models/Cart.java
@@ -2,8 +2,10 @@ package org.jumia.data.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +14,13 @@ import java.util.List;
 public class Cart {
     @Id
     private String id;
+
     private String userId;
-    private List<String> productIds = new ArrayList<>();
+
+    @DBRef
+    private List<CartItem> items = new ArrayList<>();
+
     private Double totalAmount;
+    private LocalDateTime lastUpdated = LocalDateTime.now();
 
 }
