bf340322a624dd69c618d5afcb11896aae4c19dd
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 00:08:49 +0100

    update

---
+++ src/main/java/org/jumia/dtos/requests/CreateOrderRequest.java
@@ -0,0 +1,24 @@
+package org.jumia.dtos.requests;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.util.List;
+
+
+
+@Data
+public class CreateOrderRequest {
+    @NotNull(message = "Total price is required.")
+    @Positive(message = "Total price must be greater than zero.")
+    private Double totalPrice;
+
+    private String orderStatus; // Optional, fallback to PENDING
+
+    @NotEmpty(message = "Products list must not be empty.")
+    private List<OrderedProductDTO> products;
+}
+
+
+
+
