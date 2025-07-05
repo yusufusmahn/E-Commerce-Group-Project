51f917fc2e7a273e276e86b16572d51955a5bdf7
yusufusman <yusuf.usman1003@gmail.com>
2025-07-05 07:19:49 +0100

    update

--- src/main/java/org/jumia/security/JwtRequestFilter.java
+++ src/main/java/org/jumia/security/JwtRequestFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jumia.data.models.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,6 +21,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtil jwtUtil;
+
+    @Autowired
+    private TokenBlacklist tokenBlacklist;
+
 
 
 //    @Override
@@ -45,6 +50,40 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 //        chain.doFilter(request, response);
 //    }
 
+
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+//            throws ServletException, IOException {
+//        final String authorizationHeader = request.getHeader("Authorization");
+//
+//        String username = null;
+//        String jwt = null;
+//
+//        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+//            jwt = authorizationHeader.substring(7);
+//            username = jwtUtil.extractUsername(jwt);
+//        }
+//
+//        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//            if (jwtUtil.validateToken(jwt, username)) {
+//                List<String> roles = jwtUtil.extractRoles(jwt);
+//
+//                List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+//                for (String role : roles) {
+
+    /// /                    authorities.add(new SimpleGrantedAuthority(role));
+//                    authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+//                }
+//
+//                SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
+//                        username, null, authorities));
+//            }
+//        }
+//
+//        chain.doFilter(request, response);
+//    }
+//
+//}
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
@@ -55,17 +94,23 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
+
+            if (tokenBlacklist.isTokenBlacklisted(jwt)) {
+                throw new SecurityException("Token has been invalidated. Please login again.");
+            }
+
             username = jwtUtil.extractUsername(jwt);
         }
 
+
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             if (jwtUtil.validateToken(jwt, username)) {
-                List<String> roles = jwtUtil.extractRoles(jwt);
+                List<Role> roles = jwtUtil.extractRoles(jwt); // Extracting List<Role>
 
+                // Convert roles to SimpleGrantedAuthority
                 List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                for (String role : roles) {
-//                    authorities.add(new SimpleGrantedAuthority(role));
-                    authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+                for (Role role : roles) {
+                    authorities.add(new SimpleGrantedAuthority("ROLE_" + role.name()));
                 }
 
                 SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
@@ -77,3 +122,4 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     }
 
 }
+
