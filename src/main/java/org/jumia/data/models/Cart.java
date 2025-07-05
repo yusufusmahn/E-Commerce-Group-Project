88f519c3d2fabf5f64bdc9c98cbbbf6c31412d5c
yusufusman <yusuf.usman1003@gmail.com>
2025-07-05 18:15:18 +0100

    update

--- src/main/java/org/jumia/data/models/Cart.java
+++ src/main/java/org/jumia/data/models/Cart.java
@@ -1,9 +1,10 @@
-package org.temu.data.models;
+package org.jumia.data.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -11,9 +12,8 @@ import java.util.List;
 public class Cart {
     @Id
     private String id;
-
     private String userId;
-    private List<String> productIds;
+    private List<String> productIds = new ArrayList<>();
     private Double totalAmount;
 
 }
