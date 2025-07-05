aeeba5b81ad5c7ee6a8aa061e4890f4fdea23127
yusufusman <yusuf.usman1003@gmail.com>
2025-07-05 07:20:07 +0100

    update

---
+++ src/main/java/org/jumia/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package org.jumia.exceptions;
+
+public class EntityNotFoundException extends JumiaException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
