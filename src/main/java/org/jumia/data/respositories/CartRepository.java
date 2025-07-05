2728b4b740d962d06040469479c873a0927270e6
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 00:08:37 +0100

    update

--- src/main/java/org/jumia/data/respositories/CartRepository.java
+++ src/main/java/org/jumia/data/respositories/CartRepository.java
@@ -3,8 +3,12 @@ package org.jumia.data.respositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.jumia.data.models.Cart;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface CartRepository extends MongoRepository<Cart, String> {
     Optional<Cart> findByUserId(String userId);
+    List<Cart> findByLastUpdatedBefore(LocalDateTime cutoff);
+
 }
