b307f266c43bc952c14c4411dc7d7bab22fb1e06
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 00:08:55 +0100

    update

---
+++ src/main/java/org/jumia/dtos/requests/CreateProductRequest.java
@@ -0,0 +1,22 @@
+package org.jumia.dtos.requests;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class CreateProductRequest {
+    @NotBlank(message = "Product name is required.")
+    private String name;
+
+    @NotBlank(message = "Product description is required.")
+    private String description;
+
+    @NotNull(message = "Price is required.")
+    @Positive(message = "Price must be greater than zero.")
+    private Double price;
+
+    private int quantityAvailable;
+
+
+}
