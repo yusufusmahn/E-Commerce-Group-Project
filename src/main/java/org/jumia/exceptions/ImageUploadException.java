5c92b9bc1eef731f085005f7d3fc3f886b561886
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 10:49:23 +0100

    update

--- src/main/java/org/jumia/exceptions/ImageUploadException.java
+++ src/main/java/org/jumia/exceptions/ImageUploadException.java
@@ -4,8 +4,5 @@ public class ImageUploadException extends JumiaException {
     public ImageUploadException(String message) {
         super(message);
     }
-//
-//    public ImageUploadException(String message, Throwable cause) {
-//        super(message, cause);
-//    }
+
 }
