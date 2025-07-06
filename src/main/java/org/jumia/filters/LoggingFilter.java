99834ef46734f9791b4c808b64abc495fa6785c0
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 10:37:46 +0100

    update

---
+++ src/main/java/org/jumia/filters/LoggingFilter.java
@@ -0,0 +1,22 @@
+package org.jumia.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        System.out.println("Request received: " + request.getRequestURI());
+        System.out.println("Content-Type: " + request.getContentType());
+        filterChain.doFilter(request, response);
+    }
+}
