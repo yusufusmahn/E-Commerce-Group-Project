a8f313e4e365aeef3317441afed71e3bb3cf69d2
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 10:38:17 +0100

    update

---
+++ src/main/java/org/jumia/controllers/auth/AuthController.java
@@ -0,0 +1,44 @@
+package org.jumia.controllers.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.jumia.dtos.requests.ForgotPasswordRequest;
+import org.jumia.dtos.requests.ResetPasswordRequest;
+import org.jumia.services.auth.LogoutService;
+import org.jumia.services.auth.PasswordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private PasswordService passwordService;
+
+    @Autowired
+    private LogoutService logoutService;
+
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> sendResetLink(@Valid @RequestBody ForgotPasswordRequest request) {
+        passwordService.sendResetLink(request);
+        return ResponseEntity.ok("Reset link sent to your email.");
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordRequest request) {
+        passwordService.resetPassword(request);
+        return ResponseEntity.ok("Password reset successful.");
+    }
+
+    // src/main/java/org/jumia/controllers/auth/AuthController.java
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        logoutService.logout(request);
+        return ResponseEntity.ok("Logged out successfully.");
+    }
+
+}
