2305932129fe041f967ec2cf1833481d41637b34
yusufusman <yusuf.usman1003@gmail.com>
2025-07-06 10:51:41 +0100

    update

--- src/main/java/org/temu/data/respositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.temu.data.respositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.temu.data.models.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
