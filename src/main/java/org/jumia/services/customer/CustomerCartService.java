673e5798f006740bc30c8d0fb3126da4d56766f7
yusufusman <yusuf.usman1003@gmail.com>
2025-07-09 09:33:47 +0100

    update

--- src/main/java/org/jumia/services/customer/CustomerCartService.java
+++ src/main/java/org/jumia/services/customer/CustomerCartService.java
@@ -1,8 +1,11 @@
 package org.jumia.services.customer;
 
 import org.jumia.dtos.requests.AddToCartRequest;
+import org.jumia.dtos.requests.GuestCartItemDTO;
 import org.jumia.dtos.requests.UpdateCartRequest;
 import org.jumia.dtos.responses.CartResponse;
+
+import java.util.List;
 
 public interface CustomerCartService {
     CartResponse getCartForCurrentUser();
@@ -10,5 +13,7 @@ public interface CustomerCartService {
     CartResponse updateCartForCurrentUser(UpdateCartRequest request);
     void removeFromCartForCurrentUser(String productId);
     void clearCartForCurrentUser();
+    void mergeGuestCart(List<GuestCartItemDTO> items);
+
 }
 
