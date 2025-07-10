614e35a309966457115ee5b590d6d7344074e87b
yusufusman <yusuf.usman1003@gmail.com>
2025-07-10 14:16:19 +0100

    update

---
+++ src/main/java/org/jumia/dtos/requests/DeleteUserRequest.java
@@ -0,0 +1,13 @@
+package org.jumia.dtos.requests;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class DeleteUserRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+}
