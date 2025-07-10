a8af9c56738e3999b987130e592b89bdd5c1d59f
yusufusman <yusuf.usman1003@gmail.com>
2025-07-10 13:33:24 +0100

    update

--- src/main/java/org/jumia/services/admin/AdminOrderServiceImpl.java
+++ src/main/java/org/jumia/services/admin/AdminOrderServiceImpl.java
@@ -1,7 +1,9 @@
 package org.jumia.services.admin;
 
+import org.jumia.data.models.OrderStatus;
 import org.jumia.data.models.Product;
 import org.jumia.data.respositories.ProductRepository;
+import org.jumia.dtos.requests.UpdateOrderStatusRequest;
 import org.jumia.dtos.responses.OrderedProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,7 @@ import org.jumia.security.CurrentUserProvider;
 import org.jumia.security.RoleValidator;
 import org.jumia.utility.Mapper;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -28,6 +31,7 @@ public class AdminOrderServiceImpl implements AdminOrderService {
 
     @Autowired
     private CurrentUserProvider currentUserProvider;
+
 
 
     @Override
@@ -85,6 +89,34 @@ public class AdminOrderServiceImpl implements AdminOrderService {
                 .orElseThrow(() -> new ResourceNotFoundException("Order not found with ID: " + id));
         orderRepository.delete(order);
     }
+
+
+    @Override
+    public void updateOrderStatus(String orderId, UpdateOrderStatusRequest request) {
+        User currentUser = currentUserProvider.getAuthenticatedUser();
+        RoleValidator.validateAdmin(currentUser);
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+
+        OrderStatus currentStatus = order.getStatus();
+        OrderStatus newStatus = request.getStatus();
+
+        // Don't allow updates if order is CANCELLED or RETURNED
+        if (currentStatus == OrderStatus.CANCELLED || currentStatus == OrderStatus.RETURNED) {
+            throw new IllegalStateException("Cannot update a cancelled or returned order.");
+        }
+
+        // Disallow going backwards in status (e.g., DELIVERED -> PROCESSING)
+        if (newStatus.ordinal() < currentStatus.ordinal()) {
+            throw new IllegalArgumentException("Invalid status transition.");
+        }
+
+        order.setStatus(newStatus);
+        order.setUpdatedAt(LocalDateTime.now());
+        orderRepository.save(order);
+    }
+
 
     @Override
     public void validateAdminRole(org.springframework.security.core.Authentication authentication) {
