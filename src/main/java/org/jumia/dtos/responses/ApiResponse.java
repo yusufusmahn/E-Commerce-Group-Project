f1f860afd55cf1b4691024c116d6599cf8f816e0
yusufusman <yusuf.usman1003@gmail.com>
2025-07-12 01:23:38 +0100

    update

--- src/main/java/org/jumia/dtos/responses/ApiResponse.java
+++ src/main/java/org/jumia/dtos/responses/ApiResponse.java
@@ -1,12 +1,15 @@
 package org.jumia.dtos.responses;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ApiResponse {
     private Object data;
     private boolean success;
 
+    public ApiResponse(Object data, boolean success) {
+        this.data = data;
+        this.success = success;
+    }
 }
+
