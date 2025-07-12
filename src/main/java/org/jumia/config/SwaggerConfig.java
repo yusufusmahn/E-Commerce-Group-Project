3c29d1796866bce72b0254682bdb92be59de8fb6
yusufusman <yusuf.usman1003@gmail.com>
2025-07-12 01:23:00 +0100

    update

---
+++ src/main/java/org/jumia/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package org.jumia.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Jumia E-Commerce API")
+                .version("1.0")
+                .description("API documentation for the Jumia E-Commerce platform."));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
