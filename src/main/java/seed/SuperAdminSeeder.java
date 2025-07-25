a4b3a996abafbfabcdb39e786e136bee0daa7730
yusufusman <yusuf.usman1003@gmail.com>
2025-07-25 13:37:38 +0100

    update

--- src/main/java/seed/SuperAdminSeeder.java
+++ src/main/java/seed/SuperAdminSeeder.java
@@ -1,41 +1,41 @@
-package seed;
-
-import org.jumia.data.models.Role;
-import org.jumia.data.models.User;
-import org.jumia.data.respositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import java.util.Set;
-
-@Component
-public class SuperAdminSeeder {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public void seed() {
-        String email = "superadmin@jumia.com";
-
-        if (userRepository.existsByEmailIgnoreCase(email)) {
-            System.out.println("Super admin already exists. Skipping...");
-            return;
-        }
-
-        User superAdmin = new User();
-        superAdmin.setName("Super Admin");
-        superAdmin.setEmail(email);
-        superAdmin.setPassword(passwordEncoder.encode("password"));
-        superAdmin.setRoles(Set.of(Role.SUPER_ADMIN));
-        superAdmin.setSuperAdmin(true);
-        superAdmin.setContactInfo("08123456789");
-        superAdmin.setDescription("Platform-level administrator");
-
-        userRepository.save(superAdmin);
-        System.out.println("Super admin user seeded: " + email);
-    }
-}
+//package seed;
+//
+//import org.jumia.data.models.Role;
+//import org.jumia.data.models.User;
+//import org.jumia.data.respositories.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Set;
+//
+//@Component
+//public class SuperAdminSeeder {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    public void seed() {
+//        String email = "superadmin@jumia.com";
+//
+//        if (userRepository.existsByEmailIgnoreCase(email)) {
+//            System.out.println("Super admin already exists. Skipping...");
+//            return;
+//        }
+//
+//        User superAdmin = new User();
+//        superAdmin.setName("Super Admin");
+//        superAdmin.setEmail(email);
+//        superAdmin.setPassword(passwordEncoder.encode("password"));
+//        superAdmin.setRoles(Set.of(Role.SUPER_ADMIN));
+//        superAdmin.setSuperAdmin(true);
+//        superAdmin.setContactInfo("08123456789");
+//        superAdmin.setDescription("Platform-level administrator");
+//
+//        userRepository.save(superAdmin);
+//        System.out.println("Super admin user seeded: " + email);
+//    }
+//}
