e7321c21f9b9cee804e8aee7a4bf5f9a19853d88
yusufusman <yusuf.usman1003@gmail.com>
2025-07-25 13:37:52 +0100

    update

--- src/main/java/org/jumia/Main.java
+++ src/main/java/org/jumia/Main.java
@@ -4,13 +4,19 @@ package org.jumia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.github.cdimascio.dotenv.Dotenv;
 
-@EnableScheduling
-@SpringBootApplication(scanBasePackages = {"org.jumia", "seed"})
 
+//@EnableScheduling
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
 
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(Main.class, args);
     }
 }
+
