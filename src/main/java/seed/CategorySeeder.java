a07bf619477cd4ac2ac2a691423219b289c33134
yusufusman <yusuf.usman1003@gmail.com>
2025-07-25 13:38:06 +0100

    update

--- src/main/java/seed/CategorySeeder.java
+++ src/main/java/seed/CategorySeeder.java
@@ -1,45 +1,45 @@
-package seed;
-
-import jakarta.annotation.PostConstruct;
-import org.jumia.data.models.Category;
-import org.jumia.data.respositories.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.List;
-
-@Component
-public class CategorySeeder {
-
-    @Autowired
-    private CategoryRepository categoryRepository;
-
-    private final List<String> defaultCategories = Arrays.asList(
-//            "ELECTRONICS",
-            "FASHION",
-            "GROCERIES",
-            "HOME_APPLIANCES",
-            "BOOKS",
-            "MOBILE_DEVICES",
-            "TOYS",
-            "BEAUTY",
-            "SPORTS",
-            "OFFICE_SUPPLIES"
-    );
-
-    public void seed() {
-        if (categoryRepository.count() > 0) {
-            System.out.println("Categories already exist. Skipping seeding...");
-            return;
-        }
-
-        for (String name : defaultCategories) {
-            Category category = new Category();
-            category.setName(name);
-            categoryRepository.save(category);
-        }
-
-        System.out.println("Seeded " + defaultCategories.size() + " categories.");
-    }
-}
+//package seed;
+//
+//import jakarta.annotation.PostConstruct;
+//import org.jumia.data.models.Category;
+//import org.jumia.data.respositories.CategoryRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Component
+//public class CategorySeeder {
+//
+//    @Autowired
+//    private CategoryRepository categoryRepository;
+//
+//    private final List<String> defaultCategories = Arrays.asList(
+////            "ELECTRONICS",
+//            "FASHION",
+//            "GROCERIES",
+//            "HOME_APPLIANCES",
+//            "BOOKS",
+//            "MOBILE_DEVICES",
+//            "TOYS",
+//            "BEAUTY",
+//            "SPORTS",
+//            "OFFICE_SUPPLIES"
+//    );
+//
+//    public void seed() {
+//        if (categoryRepository.count() > 0) {
+//            System.out.println("Categories already exist. Skipping seeding...");
+//            return;
+//        }
+//
+//        for (String name : defaultCategories) {
+//            Category category = new Category();
+//            category.setName(name);
+//            categoryRepository.save(category);
+//        }
+//
+//        System.out.println("Seeded " + defaultCategories.size() + " categories.");
+//    }
+//}
